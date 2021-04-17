676537bd0d028914bf3a979583a7f7dc4cf0d1e3
choi-ys <project.log.062@gmail.com>
2021-04-18 01:00:23 +0900

    [2021-04-18]용석:store를 이용한 데이터 핸들링
     - actions : axios를 이용해 정의된 API 통신 모듈 호출 부
     - mutations : actions 계층의 API 응답 데이터를 state에 저장
     - state : mutations에 의해 commit된 API 응답 데이터의 저장소
     - dispatch : store에서 제공하는 해당 함수를 이용하여 각 컴포넌트에서 actions 계층에 적용된 API 통신 모듈 호출

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchAsksList } from '../api/index.js'
 
 // import example from './module-example'
 
@@ -16,6 +17,44 @@ Vue.use(Vuex)
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
+    /**
+     * mutations에 의해 commit된 API 응답 데이터의 저장소
+     *  - actions를 이용해 호출된 API의 응답값이 mutations을 통해 satate에 저장
+     */
+    state:{
+      askList: []
+    },
+    /**
+     * axios를 이용해 정의된 API 통신 모듈 호출 부
+     */
+    actions:{
+      FETCH_ASK(context){
+        /**
+         * javascript의 Arrow Fucntion을 이용하여
+         * axios의 this가 아닌 vue component의 매개변수에 접근
+         * 문법 : () => {}
+         */
+        fetchAsksList()
+          .then((response =>{
+            /**
+             * actions계층에서 제공하는 context를 이용하여 mutations에 정의된 함수 호출
+             * context(mutations함수명, 전달 Data)
+             */
+            context.commit('SET_ASK', response.data)
+          }))
+          .catch((error =>{
+            console.log(error);
+          }))
+      }
+    },
+    /**
+     * actions 계층의 API 응답 데이터를 state에 저장
+     */
+    mutations:{
+      SET_ASK(state, data){
+        state.askList = data;
+      }
+    },
     modules: {
       // example
     },
